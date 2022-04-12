fc8a6287cf1a2f691a9b962ff41cdfdf5a0144e8
GustavoJoiaP <joia.gustavo@gmail.com>
2022-04-11 21:25:01 -0300

    First commit: Foi criado uma API para consumir dados de filmes, foi criado arquivos de Controller com uma base doi que será consumido, um arquivo Model para modelar o banco, Data para fazer a conexão ao banco e Migration para realizar a construção do banco MySQL.

---
+++ Models/Filme.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesAPI.Models
+{
+    public class Filme
+    {
+        [Key]
+        [Required]
+        public int Id { get;  set; }
+
+        [Required(ErrorMessage = "O campo Titulo é obrigatorio")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "O campo Diretor é obrigatorio")]
+        public string Diretor { get; set; }
+        [StringLength(30, ErrorMessage ="O Genero do filme nao pode passar de 30 caracteres")]
+        public string Genero { get; set; }
+        [Range(1, 300, ErrorMessage = "A duração do filme tem que ter no minimo 1 e no max 300 minutos")]
+        public int Duracao { get; set; }
+        
+    }
+}
