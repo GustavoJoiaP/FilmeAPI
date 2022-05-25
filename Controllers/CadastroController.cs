878c6e98e452f785ff03868e839281a0f25948c9
Gustavo Joia Paziani <joia.gustavo@gmail.com>
2022-05-25 11:25:20 -0300

    Segundo commit UsuarioAPI: Foi feita a conexão com o banco de dados e criado uma tabela no banco com o nome de usuarioDb e a migration

---
+++ Controllers/CadastroController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UsuariosAPI.Data.DataTransferObject.UsuarioDTO;
+
+namespace UsuariosAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CadastroController: ControllerBase
+    {
+        [HttpPost]
+        public IActionResult AdicionarUsuario([FromBody] CreateUsuarioDTO usuarioDTO)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Profiles/UsuarioProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UsuariosAPI.Data.DataTransferObject.UsuarioDTO;
+using UsuariosAPI.Model;
+
+namespace UsuariosAPI.Profiles
+{
+    public class UsuarioProfile : Profile
+    {
+        public UsuarioProfile()
+        {
+            CreateMap<CreateUsuarioDTO, Usuario>();
+            
+        }
+    }
+}
