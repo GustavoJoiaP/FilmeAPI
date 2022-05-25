5ded7006efb8a17ee0f0356d3f2067ba4cb51810
Gustavo Joia Paziani <joia.gustavo@gmail.com>
2022-05-25 14:56:54 -0300

    teste commit

---
+++ Services/UsuarioService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UsuariosAPI.Services
+{
+    public class UsuarioService
+    {
+    }
+}
